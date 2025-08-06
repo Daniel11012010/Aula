7378d0e319f8a33bb79689559eb9dcb42af3d6d2
Daniel11012010 <88914876+Daniel11012010@users.noreply.github.com>
2025-08-06 11:06:49 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade: "))
+
+frasedoseudia = input("Digite uma frase do seu dia:")
+A = int(input("Digite um número: "))
+B = int(input("Digite outro número: "))
+Soma = A + B
+
+print(nome, idade, frasedoseudia,)
+print(A, B, Soma)
